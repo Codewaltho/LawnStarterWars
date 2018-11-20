0da01a094fd1fafdacd79c85974717deece587b4
Codewaltho <37387815+Codewaltho@users.noreply.github.com>
2018-11-20 15:49:19 -0600

    Update Main.js
    Started definition of Main.js. Need to render, but time limit has been reached

--- src/views/Main.js
+++ src/views/Main.js
@@ -4,7 +4,28 @@ import SearchBar from "./SearchBar";
 import Results from "./Results";
 
 export default class Home extends Component {
-  constructor(props) {}
+  constructor(props) {
+    super(props) {
+    this.satte = {SearchResults: [], nowSearching: false, category: ""};
+    this.Search = this.Search.bind(this);
+    }
+
+  axios({
+      method: "get",
+      url: `https://swapi.co/api/${
+        category === "movies" ? "films" : category}/?search=${searched}`
+    })
+      .then(res => {
+        this.setState({
+          currentlySearching: false,
+          SearchResults: res.data.results
+        });
+      })
+      .catch(err => {
+        console.log(err.data);
+      });
+  })
+  }
 }
 
-//Use axios for URL. import SearchBar and Results to
+//render statement -- pausing because time limit reached
